0a0ef5a42939e3079f4fb5da22b7dc6a78120cc8
Gabriel Luz <103142339+gabrielhluz@users.noreply.github.com>
2022-12-17 22:57:23 -0300

    Delete senha.js

--- site-procurado/script/senha.js
+++
@@ -1,7 +0,0 @@
-var tSenha, senha
-senha = "gabriel100sapato"
-if (!localStorage.getItem("logado")){
-while(tSenha !== senha){
-    tSenha = window.prompt(`DIGITE A SENHA:`)
-    tSenha !== senha ? window.alert(`SENHA INCORRETA!`) : localStorage.setItem("logado",true) 
-}}
